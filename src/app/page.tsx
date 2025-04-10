ff09bb7b3feac96ded9425b9c6579729266d8c33
syarifhidayattuloh <syarifhidayattuloh752@gmail.com>
2025-04-10 15:05:47 +0700

    02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import TodoList from '@/components/todolist';
 import React from 'react';
 
 function page() {
   return (
     <>
-      <h1>Landing Page </h1>
+      <TodoList />
     </>
   );
 }
